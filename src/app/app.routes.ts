d23a591cceead708affaef7dfb09602cfed2ce1f
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-05-05 22:34:03 -0600

    Configuracion inicial de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'post', component: PostComponent}, 
+    {path: 'catalog', component: CatalogComponent},
+];
