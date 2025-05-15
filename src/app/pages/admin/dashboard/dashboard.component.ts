154c16bb690732cda3d3519ed0e0160292c2fd06
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-05-14 21:43:07 -0600

    Nueva Pagina de nuevos productos solo front

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarAdminComponent } from '../../../component/admin/sidebar-admin/sidebar-admin.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SidebarAdminComponent],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
