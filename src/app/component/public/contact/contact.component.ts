b7b17294ef307f5f4e0aff2519d66c6a6907fd6c
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-06-06 18:54:41 -0600

    correo

--- src/app/component/public/contact/contact.component.ts
+++ src/app/component/public/contact/contact.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+
+ngAfterViewInit() {
+  const form = document.getElementById('contacto-form') as HTMLFormElement;
+  const modalElement = document.getElementById('exitoModal');
+
+  if (!form || !modalElement) return;
+
+  form.addEventListener("submit", () => {
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+
+    // Oculta el modal luego de 4 segundos
+    setTimeout(() => modal.hide(), 4000);
+
+    // Limpia los campos luego del envío
+    setTimeout(() => form.reset(), 2000);
+  });
+}
 
 }
